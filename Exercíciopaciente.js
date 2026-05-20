a7854ecdbe2191cb7d1e1e23c5101d4ca1d5a001
Yamada <igoryamada57@gmail.com>
2026-05-20 13:22:08 -0300

    Create Exercíciopaciente.js

---
+++ Exercíciopaciente.js
@@ -0,0 +1,77 @@
+/ Classe Paciente
+class Paciente {
+    constructor(nome, senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+}
+
+// Classe FilaAtendimento
+class FilaAtendimento {
+
+    constructor() {
+        this.fila = [];
+        this.contadorSenha = 1;
+    }
+
+    adicionarPaciente(nome) {
+
+        let paciente = new Paciente(
+            nome,
+            this.contadorSenha
+        );
+
+        this.fila.push(paciente);
+
+        console.log(
+            `Paciente ${nome} entrou na fila com a senha ${this.contadorSenha}`
+        );
+
+        this.contadorSenha++;
+    }
+
+    chamarPaciente() {
+
+        if (this.fila.length === 0) {
+            console.log("Não há pacientes na fila.");
+            return;
+        }
+
+        let paciente = this.fila.shift();
+
+        console.log(
+            `Chamando paciente: ${paciente.nome} | Senha: ${paciente.senha}`
+        );
+    }
+
+    mostrarFila() {
+
+        if (this.fila.length === 0) {
+            console.log("Fila vazia.");
+            return;
+        }
+
+        console.log("Fila de atendimento:");
+
+        for (let i = 0; i < this.fila.length; i++) {
+
+            console.log(
+                `Nome: ${this.fila[i].nome} | Senha: ${this.fila[i].senha}`
+            );
+        }
+    }
+}
+
+// Criando a fila
+let atendimento = new FilaAtendimento();
+
+// Testes
+atendimento.adicionarPaciente("Yamada");
+atendimento.adicionarPaciente("Maria");
+atendimento.adicionarPaciente("bia");
+
+atendimento.mostrarFila();
+
+atendimento.chamarPaciente();
+
+atendimento.mostrarFila();
