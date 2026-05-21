032941498cabb15d0811d4ec20a229d5ea1d4d6f
Yamada <igoryamada57@gmail.com>
2026-05-21 18:30:19 -0300

    Exemplos com for e while

---
+++ for-e-while/.js
@@ -0,0 +1,16 @@
+let frutas = ["Maçã", "Banana", "Uva"];
+let i = 0;
+
+while (i < frutas.length) {
+    console.log(frutas[i]);
+    i++;
+}
+/// Usando For:
+
+let soma = 0;
+
+for (let i = 1; i <= 5; i++) {
+    soma += i;
+}
+
+console.log("Soma:", soma);
