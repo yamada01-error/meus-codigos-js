a1c2365ecc1813d9de58cbbdc760712490436060
Yamada <igoryamada57@gmail.com>
2026-05-22 10:06:14 -0300

    Criar ranking.js

---
+++ exercicios-js/ranking.js
@@ -0,0 +1,32 @@
+class Turma {
+    constructor(nomeTurma) {
+        this.nomeTurma = nomeTurma;
+        this.alunos = [];
+    }
+
+    criaRanking(disciplina) {
+        let ranking = [];
+
+        for (let i = 0; i < this.alunos.length; i++) {
+            let aluno = this.alunos[i];
+            let media = aluno.calcularMedia(disciplina); 
+
+            ranking.push({
+                nome: aluno.nome,
+                media: media
+            });
+        }
+
+        for (let i = 0; i < ranking.length; i++) {
+            for (let j = 0; j < ranking.length - 1 - i; j++) {
+                if (ranking[j].media < ranking[j + 1].media) {
+                    let temp = ranking[j];
+                    ranking[j] = ranking[j + 1];
+                    ranking[j + 1] = temp;
+                }
+            }
+        }
+
+        return ranking;
+    }
+}
