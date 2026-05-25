c3bea98bf52106452e88870c21907177d4f53526
Yamada <igoryamada57@gmail.com>
2026-05-24 22:15:55 -0300

    Criar exercicios-if-else/02-maior-de-idade.js

---
+++ exercicios-if-else/02-maior-de-idade.js
@@ -0,0 +1,9 @@
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        return "Maior de idade";
+    } else {
+        return "Menor de idade";
+    }
+}
+
+console.log(verificarIdade(16));
