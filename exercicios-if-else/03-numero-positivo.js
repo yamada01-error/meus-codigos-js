7c5ac56c4bfec2d314f133c96147c3ff3d4abff1
Yamada <igoryamada57@gmail.com>
2026-05-24 22:17:35 -0300

    Criar exercicios-if-else/03-numero-positivo.js

---
+++ exercicios-if-else/03-numero-positivo.js
@@ -0,0 +1,9 @@
+function verificarNumero(numero) {
+    if (numero > 0) {
+        return "Número positivo";
+    } else {
+        return "Número negativo";
+    }
+}
+
+console.log(verificarNumero(-5));
